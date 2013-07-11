5442802998571d669f07a142f1446595bc60b6e2
Lev Taydakov <lev.taydakov@gmail.com>
2013-07-11 13:16:53 +0400

    final version

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var text = fs.readFileSync("index.html");
+var text = fs.readFileSync("index.html").toString();
 
 app.get('/', function(request, response) {
   response.send(text);
